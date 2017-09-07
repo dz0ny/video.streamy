e96669539a0430dd1164936ef7d7bb08506d3fe0
dz0ny <dz0ny@ubuntu.si>
2017-09-07 20:05:28 +0200

    Add popcorntime

--- resources/lib/plugin.py
+++ resources/lib/plugin.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import json
 import logging
 from os import path
 from urllib import unquote, urlencode
@@ -39,20 +40,30 @@ class fetchapi(object):
             }
         )
 
-    def get_listing(self, cat):
-        data = cache.get('ppc_{}'.format(cat))
-        if 1:
-            req = self.r.get(self.BASE_URL + '/{}'.format(cat))
-            raise Exception(req.url)
+    def get_movies(self):
+        data = []
+        for x in range(1, 10):
+            req = self.r.get(
+                self.BASE_URL + '/movies/{}?sort=trending'.format(x))
             req.raise_for_status()
-            movies = []
-            
-            for x in req.json():
-                req = self.r.get(self.BASE_URL + x)
-                req.raise_for_status()
-                movies.extend(req.json())
-            cache.set('ppc_{}'.format(cat), data)
+            data.extend(req.json())
         return data
+
+    def get_shows(self):
+        data = []
+        for x in range(1, 10):
+            req = self.r.get(
+                self.BASE_URL + '/shows/{}?sort=trending'.format(x))
+            req.raise_for_status()
+            data.extend(req.json())
+        return data
+
+    def get_show(self, id):
+        req = self.r.get(
+            self.BASE_URL + '/show/{}'.format(id))
+        req.raise_for_status()
+        return req.json()
+
 
 
 class torapi(object):
@@ -64,14 +75,19 @@ class torapi(object):
         if not self.token_:
             TOKEN_URL = self.BASE_URL + "?get_token=get_token"
             request = requests.get(TOKEN_URL)
+            req.raise_for_status()
             self.token_ = request.json()["token"]
         return self.token_
 
     def category(self, c):
-        url = "&min_seeders=5&min_leechers=5&sort=seeders&limit=100&category=" + c
-        req = requests.get(self.BASE_URL + "?token=" + self.token + url)
-        req.raise_for_status()
-        return req.json()
+        data = cache.get('rarbgc_{}'.format(c))
+        if not data:
+            url = "&sort=seeders&limit=100&category=" + c
+            req = requests.get(self.BASE_URL + "?token=" + self.token + url)
+            req.raise_for_status()
+            cache.set('rarbgc_{}'.format(c), req.text)
+            return req.json()
+        return json.loads(data)
 
     def search(self, c):
         url = "&sort=seeders&limit=100&mode=search&search_string=" + c
@@ -84,6 +100,7 @@ def str_url(sec):
     p = path.join(kodiutils.get_setting('server'), sec)
     return p
 
+
 def ping():
     try:
         req = requests.get(str_url('ping'))
@@ -93,8 +110,11 @@ def ping():
         kodiutils.notification("Cannot connect to server", str(e))
         return False
 
+
 def directory(url, title, arg=None):
-    addDirectoryItem(plugin.handle, plugin.url_for(url, arg) if arg else plugin.url_for(url), ListItem(title), True)
+    addDirectoryItem(plugin.handle, plugin.url_for(url, arg)
+                     if arg else plugin.url_for(url), ListItem(title), True)
+
 
 @plugin.route('/')
 def index():
@@ -107,15 +127,15 @@ def index():
     directory(rarbg_all, 'RarBG')
     directory(popcorn_all, 'PopcornTime')
     endOfDirectory(plugin.handle)
-    
+
 
 @plugin.route('/popcorn_all')
 def popcorn_all():
-    directory(pocorn_cat, 'Movies', 'movies')
-    directory(pocorn_cat, 'TV', 'shows')
-    directory(pocorn_cat, 'Anime', 'animes')
+    directory(pocorn_movies, 'Movies')
+    directory(popcorn_shows, 'TV')
+    #directory(pocorn_cat, 'Anime', 'animes')
     endOfDirectory(plugin.handle)
-    
+
 
 @plugin.route('/rarbg_all')
 def rarbg_all():
@@ -126,6 +146,7 @@ def rarbg_all():
     directory(rarbg_search, 'Search')
     endOfDirectory(plugin.handle)
 
+
 @plugin.route('/shows_public')
 def show_shows_all():
     try:
@@ -135,12 +156,13 @@ def show_shows_all():
         for t in tree.iter('item'):
             title = t.find('title').text
             ih = t.find('link').text
-            addDirectoryItem(plugin.handle, plugin.url_for(show_torrent, ih='add', magnet=ih), ListItem(title), True)
+            addDirectoryItem(plugin.handle, plugin.url_for(
+                show_torrent, ih='add', magnet=ih), ListItem(title), True)
         endOfDirectory(plugin.handle)
     except Exception as e:
         kodiutils.notification("Cannot connect to server", str(e))
         return
-    
+
 
 @plugin.route('/torrents')
 def show_torrents():
@@ -148,7 +170,8 @@ def show_torrents():
         req = requests.get(str_url('torrents'))
         req.raise_for_status()
         for t in req.json():
-            addDirectoryItem(plugin.handle, plugin.url_for(show_torrent, t['ih']), ListItem(t['name']), True)
+            addDirectoryItem(plugin.handle, plugin.url_for(
+                show_torrent, t['ih']), ListItem(t['name']), True)
         endOfDirectory(plugin.handle)
     except Exception as e:
         kodiutils.notification("Cannot connect to server", str(e))
@@ -166,44 +189,118 @@ def show_torrent(ih=None):
             req = requests.get(str_url('torrent/{}'.format(ih)))
         req.raise_for_status()
         for f in req.json()['files']:
-            addDirectoryItem(plugin.handle, "{}{}".format(kodiutils.get_setting('server'), f['data']), ListItem('/'.join(f['Path'])))
+            addDirectoryItem(plugin.handle, "{}{}".format(
+                kodiutils.get_setting('server'), f['data']), ListItem('/'.join(f['Path'])))
         endOfDirectory(plugin.handle)
     except Exception as e:
         kodiutils.notification('Request', str(e))
         return
+
 
 @plugin.route('/rarbg/<c>')
 def rarbgc(c):
     try:
         t = torapi()
         for f in t.category(c)['torrent_results']:
-            addDirectoryItem(plugin.handle, plugin.url_for(show_torrent, ih='add', magnet=f['download']), ListItem(f['filename']), True)
+            addDirectoryItem(
+                plugin.handle,
+                plugin.url_for(show_torrent, ih='add', magnet=f['download']),
+                ListItem(f['filename']), True)
         endOfDirectory(plugin.handle)
     except Exception as e:
         kodiutils.notification("rarbg", str(e))
         return
 
-
-@plugin.route('/pocorn_cat/<c>')
-def pocorn_cat(c):
+@plugin.route('/popcorn_shows')
+def popcorn_shows():
     try:
         t = fetchapi()
-        for f in t.get_listing(c):
-            li = xbmcgui.ListItem(label=f['title'])
-            li.setArt({'poster': f['images']['poster'], 'fanart': f['images']['fanart']})
-            li.setInfo(
-                year=f['year'],
-                plot=f['synopsis'],
-                mediatype='movie',
-                code=f['imdb_id'],
+        for f in t.get_shows():
+            li = ListItem(label=f['title'])
+            try:
+                li.setArt({
+                    'poster': f['images']['poster'],
+                    'fanart': f['images']['fanart']
+                })
+            except Exception:
+                pass
+            addDirectoryItem(
+                plugin.handle,
+                plugin.url_for(popcorn_show, f['imdb_id']),
+                li,
+                True
             )
-            mag = f['torrents']['en']['1080p']['url']
-            addDirectoryItem(plugin.handle, plugin.url_for(show_torrent, ih='add', magnet=mag), li, True)
         endOfDirectory(plugin.handle)
     except Exception as e:
-        kodiutils.notification("Cannot connect to server", str(e))
+        kodiutils.notification("popcorn", str(e))
         return
 
+
+@plugin.route('/popcorn_show/<id>')
+def popcorn_show(id):
+    t = fetchapi()
+    show = t.get_show(id)
+    episodes = sorted(show['episodes'], key=lambda f: (10*int(f['season']))+int(f['episode']))
+    for f in episodes:
+        try:
+            li = ListItem(label='{} S{:02d}E{:02d}: {}'.format(
+                show['title'],int(f['season']),int(f['episode']),f['title']
+            ))
+
+            try:
+                li.setInfo(
+                    plot=f['overview'],
+                )
+            except Exception:
+                pass
+            for k, v in f['torrents'].iteritems():
+                mag = v['url']
+                break
+
+            addDirectoryItem(plugin.handle, plugin.url_for(
+                show_torrent, ih='add', magnet=mag), li, True)
+        except Exception:
+            pass
+    endOfDirectory(plugin.handle)
+
+
+@plugin.route('/pocorn_movies')
+def pocorn_movies():
+    t = fetchapi()
+    for f in t.get_movies():
+        li = ListItem(label=f['title'])
+        try:
+            li.setArt({
+                'poster': f['images']['poster'],
+                'fanart': f['images']['fanart']
+            })
+        except Exception:
+            pass
+        try:
+            li.setInfo(
+                year=f['year'],
+            )
+        except Exception:
+            pass
+        try:
+            li.setInfo(
+                plot=f['synopsis'],
+            )
+        except Exception:
+            pass
+        try:
+            li.setInfo(
+                code=f['imdb_id'],
+            )
+        except Exception:
+            pass
+        for k, v in f['torrents']['en'].iteritems():
+            mag = v['url']
+            break
+
+        addDirectoryItem(plugin.handle, plugin.url_for(
+            show_torrent, ih='add', magnet=mag), li, True)
+    endOfDirectory(plugin.handle)
 
 
 @plugin.route('/videos')
@@ -212,7 +309,8 @@ def show_videos():
         req = requests.get(str_url('torrents'))
         req.raise_for_status()
         for t in req.json():
-            addDirectoryItem(plugin.handle, "{}{}".format(kodiutils.get_setting('server'), t['urls']['play']), ListItem(t['name']))
+            addDirectoryItem(plugin.handle, "{}{}".format(
+                kodiutils.get_setting('server'), t['urls']['play']), ListItem(t['name']))
         endOfDirectory(plugin.handle)
     except Exception as e:
         kodiutils.notification("Cannot connect to server", str(e))
@@ -229,7 +327,8 @@ def rarbg_search():
             try:
                 t = torapi()
                 for f in t.search(keyboardinput)['torrent_results']:
-                    addDirectoryItem(plugin.handle, plugin.url_for(show_torrent, ih='add', magnet=f['download']), ListItem(f['filename']), True)
+                    addDirectoryItem(plugin.handle, plugin.url_for(
+                        show_torrent, ih='add', magnet=f['download']), ListItem(f['filename']), True)
                 endOfDirectory(plugin.handle)
             except Exception as e:
                 kodiutils.notification("rarbg", str(e))
